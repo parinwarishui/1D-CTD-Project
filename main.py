39a6914efb76b074b12cb360be2fa64054bb6d94
Vay134 <71159266+Vay134@users.noreply.github.com>
2024-12-05 23:25:54 +0800

    j

--- main.py
+++ main.py
@@ -28,12 +28,20 @@ class MainApp(tk.Tk):
         self.main_container.columnconfigure(0, weight = 1) # Configure main_container grid of row 0 with weight 1
 
         self.goto_page(StartPage)
+        #self.bind("w", lambda e :self.page.game_canvas.move_forward())
+        #print(isinstance(self.page, StartPage))
 
     def goto_page(self, page_to_goto):
         for child in self.main_container.winfo_children():
             child.destroy()
-        page = page_to_goto(self.main_container, self)
-        page.grid(column = 0, row = 0, sticky = "nsew")
+        self.page = page_to_goto(self.main_container, self)
+        self.page.grid(column = 0, row = 0, sticky = "nsew")
+        if isinstance(self.page, GamePage):
+            self.bind_movement_keys()
+
+    def bind_movement_keys(self):
+        self.bind("<KeyPress>", lambda event : self.page.game_canvas.move(event.keysym))
+        self.bind("<KeyRelease>", lambda event : self.page.game_canvas.remove_released_keys(event.keysym))
 
 class StartPage(ttk.Frame):
     def __init__(self, parent, main_app: MainApp):
@@ -45,7 +53,6 @@ class StartPage(ttk.Frame):
         play_button = ttk.Button(self, text = "Play!", command = lambda : main_app.goto_page(GamePage)) # Play button
         play_button.grid(column = 1, row = 1, sticky = "n", padx = 5, pady = 5) # Place play button
 
-        # Resizing behavior
         self.columnconfigure(0, weight = 1)
         self.columnconfigure(2, weight = 1)
         self.rowconfigure(0, weight = 1)
@@ -58,8 +65,8 @@ class GamePage(ttk.Frame):
         question = ttk.Label(self, text = "qn", justify = "center", font = ("TkDefaultFont", 30))
         question.grid(column = 0, row = 0)
         
-        game_canvas = GameCanvas(self)
-        game_canvas.grid(column = 2, row = 0, sticky = "nsew", rowspan = 3)
+        self.game_canvas = GameCanvas(self)
+        self.game_canvas.grid(column = 2, row = 0, sticky = "nsew", rowspan = 3)
 
         info_frame = ttk.Frame(self)
         info_frame.grid(column = 0, row = 2, sticky = "nsew")
@@ -80,13 +87,53 @@ class GameCanvas(tk.Canvas):
         super().__init__(parent, borderwidth = -2) # Initialize GameCanvas as a child class of tk.Canvas
         self.t = turtle.RawTurtle(self) # Initialize a RawTurtle object as a child of GameCanvas
         self.t.penup()
-        self.focus_set()
-    
-    def move_forward(self):
-        print("asdf")
-        self.t.setheading(90)
+        self.t.speed(0)
+        self.pressed_keys = {"w" : False, "d" : False, "s" : False, "a" : False}
+            
+    def move(self, pressed_key):
+        """
+        if pressed_key not in self.pressed_keys:
+            return
+        """
+        self.pressed_keys[pressed_key] = True
+        """
+        if self.pressed_keys["w"] and self.pressed_keys["s"]:
+            self.pressed_keys["w"], self.pressed_keys["s"] = False, False
+        if self.pressed_keys["d"] and self.pressed_keys["a"]:
+            self.pressed_keys["d"], self.pressed_keys["a"] = False, False
+        
+        if True not in self.pressed_keys.values():
+            return
+        """
+        if self.pressed_keys["w"] and self.pressed_keys["d"]:
+            self.t.setheading(45)
+        elif self.pressed_keys["w"] and self.pressed_keys["a"]:
+            self.t.setheading(135)
+        elif self.pressed_keys["s"] and self.pressed_keys["a"]:
+            self.t.setheading(225)
+        elif self.pressed_keys["s"] and self.pressed_keys["d"]:
+            self.t.setheading(315)
+        elif self.pressed_keys["w"]:
+            self.t.setheading(90)
+        elif self.pressed_keys["a"]:
+            self.t.setheading(180)
+        elif self.pressed_keys["s"]:
+            self.t.setheading(270)
+        elif self.pressed_keys["d"]:
+            self.t.setheading(0)
+        else:
+            return
+        print(self.t.heading())
+        print(self.pressed_keys)
         self.t.forward(10)
+        return
+    
+    def remove_released_keys(self, released_key):
+        if released_key not in self.pressed_keys:
+            return
+        self.pressed_keys[released_key] = False
+        return
 
 MainGame = MainApp()
-MainGame.bind("w", lambda w: MainGame.pages[GamePage].game_canvas.move_forward()) # error
+#MainGame.bind("w", lambda w: MainGame.pages[GamePage].game_canvas.move_forward()) # error
 MainGame.mainloop()
